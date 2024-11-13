2d2ced24a518fa22b1e4e70a5d00cbfb0a1a1f3d
יעקב מ. פינס <74414287+YMP112@users.noreply.github.com>
2024-11-13 12:59:07 +0200

    Update library.js

--- library.js
+++ library.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const user = require.main.require('./src/user');
+const db = require.main.require('./src/database');
 
 module.exports = {
   init: function (params, callback) {
@@ -20,6 +21,21 @@ module.exports = {
       userData.customFields[fieldName] = fieldValue;
 
       user.updateUserData(uid, userData, callback);
+    });
+  },
+
+  // API route to get custom fields for a user
+  getCustomFields: function (req, res, next) {
+    const uid = req.params.uid;
+
+    user.getUserData(uid, (err, userData) => {
+      if (err) {
+        return res.status(500).send({ error: 'Could not retrieve user data' });
+      }
+
+      // Return the custom fields if they exist
+      const customFields = userData.customFields || {};
+      res.json({ customFields: customFields });
     });
   }
 };
