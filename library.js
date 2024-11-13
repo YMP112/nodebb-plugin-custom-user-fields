3d162ede8f2cfdb866b45efad208a838f15fbdf5
יעקב מ. פינס <74414287+YMP112@users.noreply.github.com>
2024-11-13 13:24:17 +0200

    Update library.js

--- library.js
+++ library.js
@@ -1,22 +1,25 @@
 'use strict';
 
 const user = require.main.require('./src/user');
+const meta = require.main.require('./src/meta');
 const db = require.main.require('./src/database');
 
 module.exports = {
+
+  // פונקציה שמאתחלת את התוסף
   init: function (params, callback) {
     console.log('Custom User Fields Plugin Initialized');
     callback();
   },
 
-  // Function to add a custom field to a user profile
+  // פונקציה להוספת שדה מותאם אישית לפרופיל המשתמש
   addCustomField: function (uid, fieldName, fieldValue, callback) {
     user.getUserData(uid, (err, userData) => {
       if (err) {
         return callback(err);
       }
       
-      // Create a customFields object if it doesn't exist
+      // אם אין שדות מותאמים אישית, ניצור אובייקט customFields
       userData.customFields = userData.customFields || {};
       userData.customFields[fieldName] = fieldValue;
 
@@ -24,7 +27,7 @@ module.exports = {
     });
   },
 
-  // API route to get custom fields for a user
+  // API לשליפת שדות מותאמים אישית של משתמש
   getCustomFields: function (req, res, next) {
     const uid = req.params.uid;
 
@@ -33,9 +36,35 @@ module.exports = {
         return res.status(500).send({ error: 'Could not retrieve user data' });
       }
 
-      // Return the custom fields if they exist
       const customFields = userData.customFields || {};
       res.json({ customFields: customFields });
+    });
+  },
+
+  // הוספת אפשרות למנהל בפאנל הניהול
+  addAdminMenu: function (header) {
+    header.plugins.push({
+      "route": '/plugins/custom-user-fields',
+      "icon": 'fa-cogs',
+      "name": 'Custom User Fields'
+    });
+    return header;
+  },
+
+  // הצגת ההגדרות בממשק הניהול של NodeBB
+  getAdminSettings: function (req, res, next) {
+    meta.settings.get('custom-user-fields', function (err, settings) {
+      res.render('admin/plugins/custom-user-fields', {
+        settings: settings || {}
+      });
+    });
+  },
+
+  // שמירת הגדרות השדות המותאמים אישית בממשק הניהול
+  saveAdminSettings: function (req, res, next) {
+    const settings = req.body;
+    meta.settings.set('custom-user-fields', settings, function () {
+      res.redirect('/admin/plugins/custom-user-fields');
     });
   }
 };
